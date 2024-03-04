097dc0ec1d38d20b687e4b46eb20af98f0f89e80
eli <ellizzabbetth@gmail.com>
2024-03-04 10:55:23 -0500

    step 2 - 5  completed

--- lamdafunction.py
+++ lamdafunction.py
@@ -10,7 +10,7 @@ logger.setLevel(logging.DEBUG)
 print('Loading Lambda function')
 
 runtime=boto3.Session().client('sagemaker-runtime')
-endpoint_Name='pytorch-inference-2024-03-03-20-03-11-422'
+endpoint_Name='pytorch-inference-2024-03-04-13-40-39-863'
 
 def lambda_handler(event, context):
 
